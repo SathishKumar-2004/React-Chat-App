25cabe83d51d5e0675a8ce7d5a173060fe4f725c
Sathish Kumar <117570195+Sathishkumar18-code@users.noreply.github.com>
2025-02-15 16:31:42 +0530

    Update commit

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,57 +1,57 @@
-import React from 'react'
-import "./detail.css"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAngleUp, faDownload } from '@fortawesome/free-solid-svg-icons'
-import { authentication, db } from '../../lib/firebase'
-import { useUserStore } from '../../lib/userStore'
-import { useChatStore } from '../../lib/chatStore'
-import { arrayRemove, arrayUnion, doc, updateDoc } from 'firebase/firestore'
+import React from "react";
+import "./detail.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleUp, faDownload } from "@fortawesome/free-solid-svg-icons";
+import { authentication, db } from "../../lib/firebase";
+import { useUserStore } from "../../lib/userStore";
+import { useChatStore } from "../../lib/chatStore";
+import { arrayRemove, arrayUnion, doc, updateDoc } from "firebase/firestore";
 
 const Detail = () => {
-  const {chatId, user,isCurrentuserBlocked,isReceiverBlocked, changeBlock}= useChatStore()
+  const { chatId, user, isCurrentuserBlocked, isReceiverBlocked, changeBlock } =
+    useChatStore();
 
-  const {currentUser} = useUserStore()
+  const { currentUser } = useUserStore();
 
-  const handleBlock = async () =>{
-    if(!user) return;
+  const handleBlock = async () => {
+    if (!user) return;
 
-    const userDocRef = doc(db,"users",currentUser.id)
+    const userDocRef = doc(db, "users", currentUser.id);
 
-    try{
-      await updateDoc(userDocRef,{
-        blocked:isReceiverBlocked ? arrayRemove(user.id):arrayUnion(user.id)
-      })
-      changeBlock()
-    }
-    catch(err){
+    try {
+      await updateDoc(userDocRef, {
+        blocked: isReceiverBlocked ? arrayRemove(user.id) : arrayUnion(user.id),
+      });
+      changeBlock();
+    } catch (err) {
       console.log(err);
     }
-  }
+  };
 
   return (
     <div className="detail">
       <div className="user">
-      <img src={user?.avatar || "./avatar.png"}alt="" />
-      <h2>{user?.username}</h2>
-      <p>Lorem ipsum dolor sit amet, </p>
+        <img src={user?.avatar || "./avatar.png"} alt="" />
+        <h2>{user?.username}</h2>
+        <p>Lorem ipsum dolor sit amet, </p>
       </div>
       <div className="info">
         <div className="option">
           <div className="title">
             <span>Chat Settings</span>
-            <FontAwesomeIcon  className="icon" icon={faAngleUp}/>
+            <FontAwesomeIcon className="icon" icon={faAngleUp} />
           </div>
         </div>
         <div className="option">
           <div className="title">
             <span>Privacy & help</span>
-            <FontAwesomeIcon className="icon"  icon={faAngleUp}/>
+            <FontAwesomeIcon className="icon" icon={faAngleUp} />
           </div>
         </div>
         <div className="option">
           <div className="title">
             <span>Shared photos</span>
-            <FontAwesomeIcon className="icon"  icon={faAngleUp}/>
+            <FontAwesomeIcon className="icon" icon={faAngleUp} />
           </div>
         </div>
         <div className="photos">
@@ -60,34 +60,42 @@ const Detail = () => {
               <img src="./avatar.png" alt="" />
               <span>photo_2024.png</span>
             </div>
-            <FontAwesomeIcon className="icon"  icon={faDownload} />
+            <FontAwesomeIcon className="icon" icon={faDownload} />
           </div>
           <div className="photoItem">
             <div className="photoDetail">
               <img src="./avatar.png" alt="" />
               <span>photo_2024.png</span>
             </div>
-            <FontAwesomeIcon className="icon"  icon={faDownload} />
+            <FontAwesomeIcon className="icon" icon={faDownload} />
           </div>
           <div className="photoItem">
             <div className="photoDetail">
               <img src="./avatar.png" alt="" />
               <span>photo_2024.png</span>
             </div>
-            <FontAwesomeIcon className="icon"  icon={faDownload} />
+            <FontAwesomeIcon className="icon" icon={faDownload} />
           </div>
-          </div>
+        </div>
         <div className="option">
           <div className="title">
             <span>Shared Files</span>
-            <FontAwesomeIcon className="icon"  icon={faAngleUp}/>
+            <FontAwesomeIcon className="icon" icon={faAngleUp} />
           </div>
         </div>
-        <button onClick={handleBlock}>{isCurrentuserBlocked ? "You are Blocked!" : isReceiverBlocked ?"User Blocked" : "Block User"}</button>
-        <button className="logout" onClick={()=>authentication.signOut()}>Logout</button>
+        <button onClick={handleBlock}>
+          {isCurrentuserBlocked
+            ? "You are Blocked!"
+            : isReceiverBlocked
+            ? "User Blocked"
+            : "Block User"}
+        </button>
+        <button className="logout" onClick={() => authentication.signOut()}>
+          Logout
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Detail
+export default Detail;
